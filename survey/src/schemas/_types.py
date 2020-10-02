e961381485d9ebc4e691c23067b6d87d7bcabc64
Mahmoudaouinti <aouintimahmod@gmail.com>
2020-10-03 00:32:01 +0100

    Feature Questionnaire *- Fix serializer issues

--- survey/src/schemas/_types.py
+++ survey/src/schemas/_types.py
@@ -4,12 +4,10 @@ from flask import escape, Markup
 
 class EscapedStr(ma.Field):
 
-    def deserialize(self, value, attr = None, data = None, **kwargs):
+    def deserialize(self, value, attr=None, data = None, **kwargs):
         field_content = super().deserialize(value, attr, data, **kwargs)
         return escape(field_content) if isinstance(field_content, str) else field_content
 
     def serialize(self, value, *args, **kwargs):
         field_content = super().serialize(value, *args, **kwargs)
-        return Markup.unescape(field_content)
-
-
+        return Markup.unescape(field_content) if field_content else None
